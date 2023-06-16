98b5eb7c320e94584427836f2e0fdd53de14384b
Evandro <evandro-dev@outlook.comm>
2023-06-15 23:17:49 -0300

    atualização projetos

---
+++ projects/17/scripts/scripts.js
@@ -0,0 +1,22 @@
+const popup = document.querySelector("#popup");
+const cancelButton = document.querySelector("#cancel-button");
+console.log("aqui")
+
+localStorage.removeItem("popupDisplayed");
+
+document.addEventListener("mouseout", (event) =>{
+    const popupDisplayed = localStorage.getItem("popupDisplayed");
+
+    if(!popupDisplayed && event.relatedTarget === null){
+
+        popup.style.display = "block"
+    }
+});
+
+
+cancelButton.addEventListener("click", ()=>{
+    popup.style.display = "none"
+
+
+    localStorage.setItem("popupDisplayed", true);
+})
